a0ac264b7141b823729e83dff7e46651e50e01fa
Allef Gomes <allefgalmeida@gmail.com>
2022-06-06 21:37:41 -0300

    feat: initialize usecase test register-user-on-mailing-list

---
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -0,0 +1,16 @@
+import { UserData } from './user-data';
+
+describe('Register user on mailing list use case', () => {
+  test('should add user with complete data to mailing list', async () => {
+    const users: UserData[] = [];
+    console.log(users)
+    // const repo: UserRepository = new InMemoryUserRepository(users);
+    // const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo);
+    // const name = "any_name";
+    // const email = "any@email.com";
+    
+    // const reponse = await usecase.registerUserOnMailingList({ name, email });
+    // const user = repo.findUserByEmail(email);
+    // expect((await user).name).toBe(name);
+  })
+});
