84bab5135a459e221f958bae3da73c23d375192b
Allef Gomes <allefgalmeida@gmail.com>
2022-06-11 16:55:06 -0300

    test: validate local part larger than 64 chars

--- src/entities/email-validation.spec.ts
+++ src/entities/email-validation.spec.ts
@@ -20,4 +20,10 @@ describe('Email Validation', () => {
     const email = 'any@email.com'
     expect(Email.validate(email)).toBeTruthy()
   })
+
+  test('should not accept local part larger than 64 chars', () => {
+    const email = 'l'.repeat(65) + '@mail.com'
+
+    expect(Email.validate(email)).toBeFalsy()
+  })
 })
