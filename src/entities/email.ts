e62fb5a2a5463d582b2d1e00105b6a55a31e5745
Allef Gomes <allefgalmeida@gmail.com>
2022-06-11 17:03:07 -0300

    test: validate email length less than 321

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -2,6 +2,8 @@ export class Email {
   static validate(email: string): boolean {
     if (!Boolean(email)) return false
 
+    if (email.length > 320) return false
+
     const [local, _domain] = email.split('@')
     if (local.length > 64) return false
 
