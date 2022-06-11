1e7ef719c492cf9e6ff40deaa5bdac99ecd2dc17
Allef Gomes <allefgalmeida@gmail.com>
2022-06-11 16:55:01 -0300

    feat: validate local part larger than 64 chars

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -1,7 +1,10 @@
 export class Email {
   static validate(email: string): boolean {
-    if (Boolean(email)) return true
+    if (!Boolean(email)) return false
 
-    return false
+    const [local, _domain] = email.split('@')
+    if (local.length > 64) return false
+
+    return true
   }
 }
