8e4e32dc1bcc1201da0166f9732a9ad45829571a
Allef Gomes <allefgalmeida@gmail.com>
2022-06-11 16:39:14 -0300

    chore: fix .eslintrc and jest config

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,9 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  coverageDirectory: "coverage",
+  coverageDirectory: 'coverage',
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
-  },
-  verbose: true
-};
+  }
+}
