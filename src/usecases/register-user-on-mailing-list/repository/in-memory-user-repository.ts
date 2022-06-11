65ab218b7cc2e02c09de65b24a13b9276b8ea79b
Allef Gomes <allefgalmeida@gmail.com>
2022-06-11 16:32:50 -0300

    feat: create repository functions to add, findUserByEmail, findAllUsers and exists user

---
+++ src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.ts
@@ -0,0 +1,36 @@
+import { UserData } from '../user-data';
+import { UserRepository } from './../ports/user-repository';
+
+export class InMemoryUserRepository implements UserRepository {
+  private repository: UserData[];
+
+  constructor(repository: UserData[]) {
+    this.repository = repository
+  }
+
+  async add(user: UserData): Promise<void> {
+    const user_exists = await this.exists(user);
+
+    if (!user_exists) {
+      this.repository.push(user)
+    }
+  }
+
+  async findUserByEmail(email: string): Promise<UserData> {
+    const found = this.repository.find(user => user.email == email)
+
+    return found || null;
+  }
+
+  async findAllUsers(): Promise<UserData[]> {
+    return this.repository;
+  }
+
+  async exists(user: UserData): Promise<boolean> {
+    if (await this.findUserByEmail(user.email) === null) {
+      return false;
+    }
+
+    return true;
+  }
+}
